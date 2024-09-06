e43fa643308de372d447da45388743c2002e678e
ernestngenest <ernestprabowo@gmail.com>
2024-09-06 12:56:30 +0700

    feat: ready deploy

--- server/app.js
+++ server/app.js
@@ -1,11 +1,17 @@
+
+if(process.env.NODE_ENV !== "production" ){
+  require("dotenv").config();
+}
 require('dotenv').config();
+
+
 const express = require('express')
 const { route } = require('./routers')
 const router = require('./routers')
 var cors = require('cors')
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 
 app.use(cors())
